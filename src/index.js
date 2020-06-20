f93a2ff7b4c909b6fda1f2afefb0012c92443499
Chirag Obhan <32812640+chiragobhan@users.noreply.github.com>
2020-06-21 00:14:34 +0530

    fixed react router bug

--- src/index.js
+++ src/index.js
@@ -8,55 +8,55 @@ import ClockClassComponent from './ClockClassComponent';
 import ClockHooksComponent from './ClockHooksComponent';
 import CrudClass from './CrudClass';
 import CrudHooks from './CrudHooks';
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
 
 function AppRoutes() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div>
-                <ul style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-                    <li style={{ margin: '20px' }}>
-                        <Link to="/itlh-react/TicTacToe">Tic Tac Toe</Link>
+                <ul style={{ display: 'flex', justifyContent: 'center', margin: '15px' }}>
+                    <li style={{ margin: '15px' }}>
+                        <Link to="/TicTacToe">Tic Tac Toe</Link>
                     </li>
-                    <li style={{ margin: '20px' }}>
-                        <Link to="/itlh-react/todoClass">ToDo Using Class</Link>
+                    <li style={{ margin: '15px' }}>
+                        <Link to="/todoClass">ToDo Using Class</Link>
                     </li>
-                    <li style={{ margin: '20px' }}>
-                        <Link to="/itlh-react/todoHooks">ToDo Using Hooks</Link>
+                    <li style={{ margin: '15px' }}>
+                        <Link to="/todoHooks">ToDo Using Hooks</Link>
                     </li>
-                    <li style={{ margin: '20px' }}>
-                        <Link to="/itlh-react/clockClass">Clock using Class</Link>
+                    <li style={{ margin: '15px' }}>
+                        <Link to="/clockClass">Clock using Class</Link>
                     </li>
-                    <li style={{ margin: '20px' }}>
-                        <Link to="/itlh-react/clockHooks">Clock using Hooks</Link>
+                    <li style={{ margin: '15px' }}>
+                        <Link to="/clockHooks">Clock using Hooks</Link>
                     </li>
-                    <li style={{ margin: '20px' }}>
-                        <Link to="/itlh-react/crudClass">Crud using Class</Link>
+                    <li style={{ margin: '15px' }}>
+                        <Link to="/crudClass">Crud using Class</Link>
                     </li>
-                    <li style={{ margin: '20px' }}>
-                        <Link to="/itlh-react/crudHooks">Crud using Hooks</Link>
+                    <li style={{ margin: '15px' }}>
+                        <Link to="/crudHooks">Crud using Hooks</Link>
                     </li>
                 </ul>
                 <Switch>
-                    <Route path="/itlh-react/TicTacToe">
+                    <Route path="/TicTacToe">
                         <TicTacToeComponent />
                     </Route>
-                    <Route path="/itlh-react/todoClass">
+                    <Route path="/todoClass">
                         <MyToDoClassComponent />
                     </Route>
-                    <Route path="/itlh-react/todoHooks">
+                    <Route path="/todoHooks">
                         <MyToDoHooksComponent />
                     </Route>
-                    <Route path="/itlh-react/clockClass">
+                    <Route path="/clockClass">
                         <ClockClassComponent />
                     </Route>
-                    <Route path="/itlh-react/clockHooks">
+                    <Route path="/clockHooks">
                         <ClockHooksComponent />
                     </Route>
-                    <Route path="/itlh-react/crudClass">
+                    <Route path="/crudClass">
                         <CrudClass />
                     </Route>
-                    <Route path="/itlh-react/crudHooks">
+                    <Route path="/crudHooks">
                         <CrudHooks />
                     </Route>
                     <Route path="/">
@@ -64,7 +64,7 @@ function AppRoutes() {
                     </Route>
                 </Switch>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
